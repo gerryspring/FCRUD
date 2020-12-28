f1abc308dd8e0e74a9764c734756cd3e1a1e05a2
vansung <luisgerardoaguilarpadilla@gmail.com>
2020-12-28 14:50:25 -0700

    from linux

---
+++ CRUD/employee.js
@@ -0,0 +1,11 @@
+class employee {
+    constructor(fname,sname,email,role,cell){
+     this.fname = fname;
+     this.sname = sname;
+     this.email = email;
+     this.role = role;
+     this.cell = cell;
+    }
+
+
+}
